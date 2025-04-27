8a8737e9c98496a02f1eccce421a748b960c4505
Marco BARNIG <mbarnig@pt.lu>
2025-04-27 17:05:43 +0200

    Update PostSwiper.jsx

--- components/PostSwiper.jsx
+++ components/PostSwiper.jsx
@@ -12,29 +12,21 @@ export default function PostSwiper({ posts }) {
       <Swiper
         spaceBetween={50}
         slidesPerView={1}
-        navigation
+        navigation={true}
         pagination={{ clickable: true }}
         effect="fade"
         fadeEffect={{ crossFade: true }}
         className="h-full"
       >
-        {posts.map(post => (
-          <SwiperSlide key={post.id}>
-            <div className="p-4 h-full overflow-y-auto">
-              <h2 className="text-2xl font-bold mb-4">{post.title.rendered}</h2>
-              <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
-            </div>
-          </SwiperSlide>
+    {posts.map(post => (
+      <SwiperSlide key={post.id} className="h-full w-full">
+        <div className="p-4 h-full w-full overflow-y-auto">
+          <h2 className="text-2xl font-bold mb-4">{post.title.rendered}</h2>
+          <div dangerouslySetInnerHTML={{ __html: post.content.rendered }} />
+        </div>
+      </SwiperSlide>
         ))}
       </Swiper>
-
-      {/* Flèches custom */}
-      <div className="swiper-button-prev absolute top-1/2 left-2 transform -translate-y-1/2 bg-gray-700 text-white rounded-full p-2 cursor-pointer z-10 hover:bg-gray-500">
-        ◀
-      </div>
-      <div className="swiper-button-next absolute top-1/2 right-2 transform -translate-y-1/2 bg-gray-700 text-white rounded-full p-2 cursor-pointer z-10 hover:bg-gray-500">
-        ▶
-      </div>
     </div>
   );
 }
